87d678a6454bf733cde5d4d09c67709179442954
lazarevkristijan <lazarevkristijan@gmail.com>
2024-08-30 20:29:40 +0300

    fix some bug

--- constants.py
+++ constants.py
@@ -2,5 +2,13 @@ APP_NAME = 'Mouse clicks automator'
 APP_LOGO_PATH = './icons/mouse.ico'
 HEIGHT = 500
 WIDTH = 500
-
+CONFIG_LAST_OPEN = 'CONFIG_LAST_OPEN.txt'
+BTN_DEAFULT_COLOR = "#333"
+BTN_HOVER_COLOR = "#444"
+TOASTER_SETTINGS = {
+    'icon_path': 'icons/mouse.ico',   # Path to an icon file (.ico)
+    'duration': 3,                    # Duration in seconds before the toast disappears
+    'threaded': True,                  # Display the toast in a separate thread
+    'sound': True,                     # Play a sound with the notification
+}
 APP_BG = 'lightblue'
